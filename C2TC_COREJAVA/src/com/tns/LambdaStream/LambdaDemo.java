c1d745dc207ec5b33c08d6f55a537e019558744f
sagar <sagar@LAPTOP-1II4FGUL>
2022-08-03 22:23:50 +0530

    Commit

---
+++ C2TC_COREJAVA/src/com/tns/LambdaStream/LambdaDemo.java
@@ -0,0 +1,19 @@
+package com.tns.LambdaStream;
+
+interface Anonymous
+{
+	void show();
+}
+
+public class LambdaDemo {
+
+	public static void main(String[] args) {
+		Anonymous l=() -> 
+		{
+			System.out.println("Lambda Demo for simplification");
+		};
+		l.show();
+
+	}
+
+}
